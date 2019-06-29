fa23b0c70d20278356ba4bf28d0bfb3ccddd1a52
Pebros <peg10@live.com.ar>
2019-06-29 17:17:16 -0300

    agrego partials errors. Se corrigieron errores de create. se agrego css.

--- Ejercicio 4/aplicacion/app/controllers/TurnosController.php
+++ Ejercicio 4/aplicacion/app/controllers/TurnosController.php
@@ -61,13 +61,29 @@ class TurnosController extends Controller
             $controles[] = "ERROR CAMPO: TELEFONO";
         }
 
-        if (!(isset($_POST['talla_de_calzado']) &&
-            is_numeric($_POST['talla_de_calzado']) &&
-            is_int((int)$_POST['talla_de_calzado']) &&
-            $this->in_range($_POST['talla_de_calzado'], 19, 46))) {
-            $controles[] = "ERROR CAMPO: TALLA DE CALZADO";
+        if(!empty($_POST['edad']))
+        {
+            if( !(is_numeric($_POST['edad'])) ||
+                !(is_int((int) $_POST['edad'])) ||
+                !($this->in_range($_POST['edad'],0,999)))
+            {
+              $controles[] = "ERROR CAMPO: EDAD";
+            }
+        }else{
+            $_POST['edad'] = "0";
         }
 
+        if(!empty($_POST['talla_de_calzado']))
+        {
+            if( !(is_numeric($_POST['talla_de_calzado'])) ||
+                !(is_int((int) $_POST['talla_de_calzado'])) ||
+                !($this->in_range($_POST['talla_de_calzado'],19,46)))
+            {
+              $controles[] = "ERROR CAMPO: TALLA DE CALZADO";
+            }
+        }else{
+            $_POST['talla_de_calzado'] = "0";
+        }
 
         if (!(isset($_POST['fecha_de_nacimiento']) &&
             $this->validar_fecha($_POST["fecha_de_nacimiento"]))) {
@@ -79,9 +95,13 @@ class TurnosController extends Controller
             $controles[] = "ERROR CAMPO: FECHA DEL TURNO";
         }
 
-        if (!(isset($_POST['horario_del_turno']) &&
-            $this->hourIsBetween('08:00', '17:00', $_POST['horario_del_turno']))) {
-            $controles[] = "ERROR CAMPO: HORARIO DEL TURNO";
+        if( !empty($_POST['horario_del_turno'])){
+            if( !$this->hourIsBetween('08:00','17:00', $_POST['horario_del_turno']))
+            {
+              $controles[] = "ERROR CAMPO: HORARIO DEL TURNO";
+            }
+        }else{
+            $_POST['horario_del_turno'] = "00:00";
         }
         //dejo por defecto una para imagenes no encontradas
         //$nombre_imagen = 'not_found.png';
@@ -129,7 +149,17 @@ class TurnosController extends Controller
             $this->model->insert($turno);
             return redirect('turnos');
         } else {
-            echo $controles['imageErr'];
+            $turno = array('nombre_del_paciente' => $_POST["nombre_del_paciente"],
+                'email'=> $_POST["email"],
+                'telefono' => $_POST["telefono"],
+                'edad' => $_POST["edad"],
+                'talla_de_calzado' => $_POST["talla_de_calzado"],
+                'altura' => $_POST["altura"],
+                'fecha_de_nacimiento' => $_POST["fecha_de_nacimiento"],
+                'color_de_pelo' => $_POST["color_de_pelo"],
+                'fecha_del_turno' => $_POST["fecha_del_turno"],
+                'horario_del_turno' => $_POST["horario_del_turno"]);
+            return view('turnos.create', compact('controles','turno'));
         }
     }
 
